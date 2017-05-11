b9b1313c035885b6e33d200fc52003c026fc0801
denissalem <denissalem@tuxfamily.org>
2017-05-11 17:49:58 +0200

    progress on hashtags

--- src/gui/assets/hashtags.js
+++ src/gui/assets/hashtags.js
@@ -6,12 +6,30 @@ function appendHashtag(a) {
 		$(".hashtag").on('click', function() {
 			$(this).remove();
 		})
+		var outputData = "";
+		ul.children("li").each(function() {
+			outputData+= $(this).text()+'|';
+		});
+		
+		alert($(textarea).attr("class"));
 	}
 	a.value = "";
 }
 
 
 $(document).ready(function() {
+
+	$(".hashtags").each( function() {
+		$(this).append("<textarea class=\"data\" name=\""+$(this).attr("ht-name")+"\">"+$(this).attr("ht-default-value")+"</textarea>");
+		ul = $("<ul class=\"displayedData\"></ul>");
+		hashtags = $(this).attr("ht-default-value").split("|");
+		for (var i=0; i < hashtags.length; i++) {
+			ul.append("<li class=\"hashtag\">"+hashtags[i]+"</li>");
+		}
+		$(this).append(ul);
+		$(this).append("<input type=\"text\" value=\"\" class=\"inputData\" />");
+	});
+
 	$(".hashtags .inputData").keypress(function( event ) {
 		if ( event.which == 13 ) {
 			event.preventDefault();
